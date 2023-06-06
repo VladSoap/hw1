489132a3770d54a1ac42ea0ebc0318a17b705e20
VladSoap <@pLUnkmdsV43TX9>
2023-06-07 00:10:09 +0300

    homework1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int timer = 0;
+        int word = 0;
+        //for
+        for (int i = 0; i < 3; i++) {
+            System.out.println("attempt - "+i);
         }
+        //While
+        while (timer < 3){
+            System.out.println(timer);
+            timer++;
+        }
+        //while do
+        do {
+            System.out.println("first homework");
+            word++;
+        }
+        while (word < 3);
+
     }
 }
